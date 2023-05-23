172c938d78690c15abde16ffdb4f4e23de08b91f
abenezertesfaye <abenezertesfaye92@gmail.com>
2023-05-23 12:14:53 -0700

    local storage added

--- script.js
+++ script.js
@@ -1,125 +1,114 @@
 const addBook = document.getElementById("add");
 const modal = document.querySelector(".modal");
-const readBook = document.querySelectorAll(".read");
 const books = document.getElementById("books");
+const form = document.querySelector("form");
 
-// modal
+// modal elements
 const title = document.getElementById("title");
 const author = document.getElementById("author");
 const pages = document.getElementById("pages");
 const readCheck = document.getElementById("readCheck");
-const form = document.querySelector("form");
 
+let library = [];
 
-const myLibrary = [
-	{
-		id: 1,
-		title: 'Mere Christianity',
-		author: 'C.S.Lewis',
-		pages: '414',
-		readCheck: false
-	},
-	{
-		id: 2,
-		title: 'Mere Christianity',
-		author: 'C.S.Lewis',
-		pages: '414',
-		readCheck: true
-	},
-	{
-		id: 3,
-		title: 'Mere Christianity',
-		author: 'C.S.Lewis',
-		pages: '414',
-		readCheck: false
-	},
-	{
-		id: 4,
-		title: 'Mere Christianity',
-		author: 'C.S.Lewis',
-		pages: '414',
-		readCheck: true
-	}
-]
-
-// myLibrary.forEach((book) => {
-// 	let div = document.createElement("div");
-// 	div.classList.add("book");
-// 	div.innerHTML = `   <p>${book.title}</p>
-// 						<p>${book.author}</p>
-// 						<p>${book.pages}</p>
-// 						<button class="read">Read</button>
-// 						<button class="remove">remove</button>
-// 					`;
-// 	books.append(div);
-// });
-
-
-// const modalcontent = document.querySelector(".modal-content");
+// open Modal when "Add Book" button is clicked
 addBook.addEventListener("click", () => {
-	title.value = "";
-	author.value = "";
-	pages.value = "";
-	readCheck.value = "";
+	resetForm();
 	modal.style.display = "block";
 });
 
+// close modal when clicking outside modal or pressing escape key
 window.addEventListener("click", (e) => {
 	if(e.target.className == "modal"){
-		modal.style.display = "none";
+		closeModal();
+	}
+});
+document.addEventListener("keydown", (e) => {
+	if(e.key == "Escape") {
+		closeModal();
 	}
 });
 
-readBook.forEach((book) => {
-	book.addEventListener("click", (e) => {
-		let bookClass = e.currentTarget.classList;
-		if(bookClass.contains("read")){
-			bookClass.replace("read", "unread");
-		    e.target.innerText = "UnRead"	
-		} else {
-			bookClass.replace("unread", "read");
-			e.target.innerText = "Read"	
-		}
-	});
-});
-
-function Book(title, author, pages, readCheck){
-	this.title = title;
-	this.author = author;
-	this.pages = pages;
-	this.readCheck = readCheck;
-}
-
+// add Book to Library
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
 	let titleVal = title.value;
 	let authorVal = author.value;
 	let pagesVal = pages.value;
-	let readCheckVal;
-	if(readCheck.checked){
-		readCheckVal = true;
-	} else {
-		readCheckVal = false;
-	}
+	let readCheckVal = readCheck.checked;
+	let id = library.length; 
+	
+	addToLibrary(id, titleVal, authorVal, pagesVal, readCheckVal);
+	saveLibraryToLocalStorage();
 
-	// let newBook = new Book();
-	// newBook.title = titleVal;
-	// newBook.author = authorVal;
-	// newBook.pages = pagesVal;
-	// newBook.readCheck = readCheckVal;
-	// myLibrary.push(newBook);
-
-	let div = document.createElement("div");
-	div.classList.add("book");
-		div.innerHTML = `
-					<p>${titleVal}</p>
-				<p>${authorVal}</p>
-					<p>${pagesVal}</p>
-					<button class="unread">Unread</button>
-					<button class="remove">remove</button>
-					`;
-	books.append(div);
-	modal.style.display = "none";
+	closeModal();
+	displayBooks();
 });
 
+// books.addEventListener("click", e => {
+// 	let target = e.target;
 
+// 	if (target.classList.contains("read") || target.classList.contains("unread")) {
+// 		const bookDiv = target.closest(".book");
+// 		const bookId = bookDiv.dataset.bookId;
+// 		const book = library.find((book) => book.id === Number(bookId));
+// 	}
+// });
+
+class Book {
+	constructor(id, title, author, pages, readCheck) {
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+		this.readCheck = readCheck;
+	}
+}
+
+function addToLibrary(id, title, author, pages, readCheck) {
+	let newBook = new Book(Number(id), title, author, pages, readCheck);
+	library.push(newBook);
+
+}
+
+function resetForm() {
+	form.reset();
+}
+function closeModal(){
+	modal.style.display = "none";
+}
+
+function displayBooks(){
+	books.innerHTML = "";
+	
+	library.forEach((book) => {
+		const div = document.createElement("div");
+		div.classList.add("book");
+	
+		const btnText = book.readCheck ? 'Read' : 'UnRead';
+		const btnClass = book.readCheck ? 'read' : 'unread';
+		
+		div.innerHTML = `<p>${book.title}</p>
+						<p>${book.author}</p>
+						<p>${book.pages}</p>
+						<button class="${btnClass}">${btnText}</button>
+						<button class="remove">remove</button>`;
+		books.append(div);
+	});
+}
+
+function saveLibraryToLocalStorage(){
+    localStorage.setItem("library", JSON.stringify(library));
+} 
+
+function loadLibraryFromLocalStorage(){
+	const libraryData = localStorage.getItem("library");
+	if(libraryData) {
+		library = JSON.parse(libraryData);
+		displayBooks();
+	}
+}
+
+
+// initial display
+document.addEventListener("DOMContentLoaded", loadLibraryFromLocalStorage);
